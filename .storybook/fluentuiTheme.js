6b9aa0c003aa79e0ab72cce495504cbc28aa9f3f
Paul Gildea <33229964+paulgildea@users.noreply.github.com>
2020-11-13 14:57:28 -0800

    Adding storybook configuration files

---
+++ .storybook/fluentuiTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Fluent UI React Converged Components',
+  brandUrl: 'https://aka.ms/fluentui',
+  brandImage: 'fluentuiLogo.svg',
+});
